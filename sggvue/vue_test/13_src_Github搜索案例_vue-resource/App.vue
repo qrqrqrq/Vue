887d80032037c6a6bbc35eb28701705fadd1b27b
joeyee <Qztwork@163.com>
2022-07-22 09:56:03 +0800

    0722 second commit

---
+++ sggvue/vue_test/13_src_Github搜索案例_vue-resource/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <TheSearch />
+    <TheList />
+  </div>
+</template>
+
+<script>
+import TheSearch from "./components/TheSearch.vue";
+import TheList from "./components/TheList.vue";
+export default {
+  name: "App",
+  components: { TheSearch, TheList },
+};
+</script>
+
+<style>
+</style>
