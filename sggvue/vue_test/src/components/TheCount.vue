4ca66ad7d8b67839f2ba4ad6ab12cb18a339c577
joeyee <Qztwork@163.com>
2022-07-25 16:33:29 +0800

    0724补提交

--- sggvue/vue_test/src/components/TheCount.vue
+++ sggvue/vue_test/src/components/TheCount.vue
@@ -26,6 +26,7 @@ export default {
     };
   },
   methods: {
+    //要和namespaced:true结合使用才能找到countOptions
     // 借助mapMutations生成对应的方法，方法中会调用commit去联系mutations（对象写法）
     ...mapMutations("countOptions", { increment: "ADD", decrement: "MINUS" }),
 
@@ -36,6 +37,7 @@ export default {
     }),
   },
   computed: {
+    //要和namespaced:true结合使用才能找到countOptions
     // 借助mapState生成计算属性，从state中读取数据。（数组写法--->取的state数据名和计算属性函数名相同用此方式）
     ...mapState("countOptions", ["sum", "school", "subject"]),
     ...mapState("personOptions", ["personList"]),
